c56e51fe5c1517b10207552eb7b0590c32493f60
DannyDelott <dannydelott@gmail.com>
2015-03-07 11:17:17 -0800

    Implement redirect in '/links' route
    - Redirect user to '/signin' if not authenticated.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,7 +1,10 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  $scope.data = {};
-  $scope.getLinks = function(){};
-  // Your code here
+.controller('LinksController', function ($scope, $location, Links, Auth) {
+  if (!Auth.isAuth()) {
+    $location.path('/signin');
+  } else {
+    $scope.data = {};
+    $scope.getLinks = function(){};
+  }
 });
