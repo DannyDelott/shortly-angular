52222559ba014c8457ac475e5bfce3d81f8e6327
DannyDelott <dannydelott@gmail.com>
2015-03-06 16:33:51 -0800

    Add link object and addLink method

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,7 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
+  $scope.link = {};
+  $scope.addLink = function(){console.log('asdf')};
   // Your code here
 });
