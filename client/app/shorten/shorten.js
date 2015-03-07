fc3f2e930c7a0ceb705037dc29ccf316031d1abe
DannyDelott <dannydelott@gmail.com>
2015-03-07 11:49:52 -0800

    Refactor ShortenController
    - Move isAuth() conditional to top to get rid of else statement.

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,13 +3,15 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links, Auth) {
   if (!Auth.isAuth()) {
     $location.path('/signin');
-  } else {
-    $scope.link = {};
-    $scope.addLink = function(){
-      Links.addLink($scope.link, function(data) {
-        console.log(data);
-        $location.path('/links');
-      });
-    };
+    return;
   }
+
+  $scope.link = {};
+  $scope.addLink = function(){
+    Links.addLink($scope.link, function(data) {
+      console.log(data);
+      $location.path('/links');
+    });
+  };
+
 });
